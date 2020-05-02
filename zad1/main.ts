ea2e6ca20d915de57d9434ac4e9d55a7aa340fbe
Piotr Jasiński <jasinskipiotr99@gmail.com>
2020-05-02 23:01:14 +0200

    quiz page and html summary page

--- zad1/main.ts
+++ zad1/main.ts
@@ -1,20 +1,20 @@
 interface Question {
-    questionText : string,
-    correctAnswer : number,
-    userAnswer : number,
-    penalty : number,
-    secondsSpend : number;
+    questionText: string,
+    correctAnswer: number,
+    userAnswer: number,
+    penalty: number,
+    secondsSpend: number;
 }
 
 interface Quiz {
-    title : string,
-    introduction : string,
-    questions : Question[];
+    title: string,
+    introduction: string,
+    questions: Question[];
 }
 
-let quizProsty : Quiz = {
-    title : "Quiz prosty",
-    introduction : "Liczyć każdy może",
+let quizProsty: Quiz = {
+    title: "Quiz prosty",
+    introduction: "Liczyć każdy może",
     questions: [
         {
             questionText: "17 + 32 - 10 = ?",
@@ -24,7 +24,7 @@ let quizProsty : Quiz = {
             secondsSpend: 0
         },
         {
-            questionText: "43 - (19 + 21) + 26",
+            questionText: "43 - (19 + 21) + 26 = ?",
             correctAnswer: 29,
             userAnswer: NaN,
             penalty: 10,
@@ -52,13 +52,20 @@ interface Stopwatch {
     seconds: number;
 }
 
+interface timeStamp {
+    minutes: number,
+    seconds: number;
+}
 
-var selectedQuiz;
+
+let selectedQuiz: Quiz = null;
 let currQuestion = 1;
 let totalQuestions = 4;
 let answeredQuestions = 0;
 let state = 0;
 let currQuestionAnswered = false;
+let stopwatch: Stopwatch = {minutes: 0, seconds: 0};
+let timestamp: timeStamp = {minutes: 0, seconds: 0};
 
 let quizTitle = document.getElementById("quizTitle") as HTMLDivElement;
 let quizIntro = document.getElementById("quizIntro") as HTMLDivElement;
@@ -68,48 +75,132 @@ let penaltyValue = document.getElementById("penaltyValue") as HTMLDivElement;
 let nextQuestButton = document.getElementById("nextQuestionButton") as HTMLSelectElement;
 let prevQuestButton = document.getElementById("prevQuestionButton") as HTMLSelectElement;
 let submitQuizButton = document.getElementById("quizSubmitButton") as HTMLSelectElement;
-let question = document.getElementById("questionAnswerField") as HTMLInputElement;
+let answer = document.getElementById("questionAnswerField") as HTMLInputElement;
+let timer;
 
-/*quizTitle.innerText = "Quiz prosty";
-quizIntro.innerText = "Liczyć każdy może";
-quizQuestion.innerText = "2 + 2 = ?";
-questionNumber.innerText = "1 / 4";
-penaltyValue.innerText = "10";*/
 prevQuestButton.style.display = "none";
 submitQuizButton.disabled = true;
 
-function loadQuestion(questionNum : number) {
-    question.value = isNaN(selectedQuiz.questions[questionNum].userAnswer) ?
-        "" : selectedQuiz.questions[questionNum].userAnswer.toString();
-    currQuestionAnswered = question.value == "";
+function resetStopwatch(stopwatch: Stopwatch) {
+    stopwatch.minutes = 0;
+    stopwatch.seconds = 0;
 }
 
-function saveAnswer(questionNum : number) {
-    selectedQuiz.questions[questionNum].userAnswer = parseInt(question.value, 10);
+function quizSummary() {
+    selectedQuiz.questions.forEach(function (question) {
+        console.log("Correct answer is: " + question.correctAnswer + ", your answer is: " + question.userAnswer);
+    })
+    document.getElementById("BodyQuizPage").style.display = "none";
+    document.getElementById("BodyQuizSummaryPage").style.display = "grid";
+
+    let tableSummary = document.getElementById("quizSummary") as HTMLTableElement;
+    let badAnswer = document.getElementById("badAnswersBox") as HTMLDivElement;
+    let totalTime = 0 as number;
+    let totalPenalty = 0 as number;
+    let headerRow = tableSummary.insertRow();
+    for (let i = 0; i < 4; i++)
+        headerRow.appendChild(document.createElement("TH"));
+    headerRow.children[0].innerHTML = "Pytanie";
+    headerRow.children[1].innerHTML = "Odpowiedź";
+    headerRow.children[2].innerHTML = "Czas";
+    headerRow.children[3].innerHTML = "Kara";
+    for (let i = 0; i < selectedQuiz.questions.length; i++) {
+        let answerRow = tableSummary.insertRow();
+        var cell = answerRow.insertCell();
+        var question = selectedQuiz.questions[i];
+        var correctAnswer = question.correctAnswer == question.userAnswer;
+        cell.innerHTML = "<span class='questionNumberSummary'>" + (i + 1).toString() + ": </span>";
+        cell.innerHTML += "<span class='questionText'>" + question.questionText + "</span>";
+        cell.classList.add("questionSummary");
+        cell = answerRow.insertCell();
+        if (correctAnswer)
+            cell.classList.add("answerOK");
+        else
+            cell.classList.add("answerBad");
+        cell.innerHTML = correctAnswer ? "OK" : "Zła odpowiedź";
+        cell = answerRow.insertCell();
+        cell.innerHTML = question.secondsSpend.toString();
+        cell = answerRow.insertCell();
+        cell.innerHTML = correctAnswer ? "0" : question.penalty.toString();
+        if (!correctAnswer) {
+            badAnswer.innerHTML += "<sup>" + (i+1).toString() + " </sup>";
+            badAnswer.innerHTML += "Twoja odpowiedź to: <span class='userAnswer'>" + question.userAnswer.toString() + "</span>";
+            badAnswer.innerHTML += ", poprawna odpowiedź to: <span class='correctAnswer'>" + question.correctAnswer.toString() + "</span>";
+            badAnswer.innerHTML += "<br>";
+        }
+
+        totalTime += question.secondsSpend;
+        totalPenalty += correctAnswer ? 0 : question.penalty;
+    }
+
+    let score = document.getElementById("scoreBox") as HTMLDivElement;
+    score.innerHTML = "Rozwiązanie quizu zajęło ci: " + convertSecondToTTimeText(totalTime) + "<br>";
+    score.innerHTML += ", kara za błędne odpowiedzi: " + convertSecondToTTimeText(totalPenalty) + "<br>";
+    score.innerHTML += "Twój lączny wynik: " + convertSecondToTTimeText(totalTime + totalPenalty);
+}
+
+function convertSecondToTTimeText(seconds : number) : string {
+    let minutes = (seconds - seconds % 60) / 60;
+    seconds -= minutes * 60;
+    let text = "";
+    if (minutes < 10)
+        text += "0";
+    text += minutes.toString() + ":";
+    if (seconds < 10)
+        text += "0";
+    text += seconds.toString();
+    return text
+}
+
+function saveTimeStamp(stopwatch : Stopwatch) {
+    timestamp.minutes = stopwatch.minutes;
+    timestamp.seconds = stopwatch.seconds;
+}
+
+function loadQuestion(questionNum: number) {
+    questionNum--;
+    answer.value = isNaN(selectedQuiz.questions[questionNum].userAnswer) ?
+        "" : selectedQuiz.questions[questionNum].userAnswer.toString();
+    //answer.placeholder = answer.value == "" ? "Twoja odpowiedź" : answer.value;
+    quizQuestion.innerText = selectedQuiz.questions[questionNum].questionText;
+    penaltyValue.innerText = selectedQuiz.questions[questionNum].penalty.toString();
+    currQuestionAnswered = answer.value != "";
+    printQuestionNumber();
+    console.log("Current question answered " + currQuestionAnswered);
+    saveTimeStamp(stopwatch);
+}
+
+function saveAnswer(questionNum: number) {
+    questionNum--;
+    selectedQuiz.questions[questionNum].userAnswer = parseInt(answer.value, 10);
+    selectedQuiz.questions[questionNum].secondsSpend += (stopwatch.seconds - timestamp.seconds);
+    selectedQuiz.questions[questionNum].secondsSpend += (stopwatch.minutes - timestamp.minutes) * 60;
 }
 
 function chooseQuiz(quiz: number) {
     console.log("Choose quiz run ");
     if (quiz === 1) {
-        selectedQuiz = quizProsty as Quiz;
+        selectedQuiz = quizProsty;
         console.log("OK");
     }
 }
 
-function loadQuiz() {
-    quizTitle.innerText =  selectedQuiz.title;
+function loadQuiz(quiz: number) {
+    if (selectedQuiz != null)
+        resetQuiz();
+    chooseQuiz(quiz);
+    document.getElementById("BodyMainPage").style.display = "none";
+    document.getElementById("BodyQuizPage").style.display = "grid";
+    quizTitle.innerText = selectedQuiz.title;
     quizIntro.innerText = selectedQuiz.introduction;
     totalQuestions = selectedQuiz.questions.length;
     currQuestion = 1;
     loadQuestion(1);
+    startTimer(stopwatch);
+    answer.addEventListener("input", checkIfAnswerInserted);
 }
 
-function addSecond(stopwatch: Stopwatch) {
-    stopwatch.seconds++;
-    if (stopwatch.seconds == 60) {
-        stopwatch.minutes++;
-        stopwatch.seconds = 0;
-    }
+function printTime() {
     let timeText: string;
     if (stopwatch.minutes < 10) {
         timeText = "0" + stopwatch.minutes.toString();
@@ -119,12 +210,20 @@ function addSecond(stopwatch: Stopwatch) {
     timeText += ":";
     timeText += stopwatch.seconds < 10 ? "0" + stopwatch.seconds.toString() : stopwatch.seconds.toString();
     document.getElementById("timerText").innerText = timeText;
-
-    timer(stopwatch);
 }
 
-function timer(stopwatch : Stopwatch) {
-    setTimeout(addSecond, 1000, stopwatch);
+function addSecond(stopwatch: Stopwatch) {
+    stopwatch.seconds++;
+    if (stopwatch.seconds == 60) {
+        stopwatch.minutes++;
+        stopwatch.seconds = 0;
+    }
+    printTime();
+    startTimer(stopwatch);
+}
+
+function startTimer(stopwatch: Stopwatch) {
+    timer = setTimeout(addSecond, 1000, stopwatch);
 }
 
 function printQuestionNumber() {
@@ -132,34 +231,29 @@ function printQuestionNumber() {
 }
 
 function checkIfAnswerInserted() {
-    if (question.value != "" && !currQuestionAnswered) {
+    if (answer.value != "" && !currQuestionAnswered) {
         currQuestionAnswered = true;
         answeredQuestions++;
     }
-    if (question.value == "" && currQuestionAnswered) {
+    if (answer.value == "" && currQuestionAnswered) {
         currQuestionAnswered = false;
         answeredQuestions--;
     }
-    console.log(answeredQuestions);
+    console.log("Answered questions is " + answeredQuestions);
     if (answeredQuestions == totalQuestions)
         submitQuizButton.disabled = false;
-}
-
-function setCurrQuestionNotAnswered() {
-    if (question.value != "") {
-        answeredQuestions--;
-        currQuestionAnswered = true;
-    } else {
-        currQuestionAnswered = false;
-    }
+    else
+        submitQuizButton.disabled = true;
 }
 
 function submitQuestion() {
     saveAnswer(currQuestion);
     if (state == 1)
         nextQuestionFunc();
-    else
+    else if (state == -1)
         prevQuestionFunc();
+    else
+        quizSummary();
 }
 
 function tryNextQuestion() {
@@ -170,30 +264,60 @@ function tryPrevQuestion() {
     state = -1;
 }
 
+function tryExitQuiz() {
+    state = 0;
+}
+
 function prevQuestionFunc() {
     if (currQuestion == totalQuestions)
         nextQuestButton.style.display = "initial";
     currQuestion--;
-    printQuestionNumber();
+    //printQuestionNumber();
     if (currQuestion == 1)
         prevQuestButton.style.display = "none";
     loadQuestion(currQuestion);
-    setCurrQuestionNotAnswered();
+}
+
+function resetQuiz() {
+    selectedQuiz.questions.forEach(function (question) {
+        question.userAnswer = NaN;
+        question.secondsSpend = 0;
+    })
+    selectedQuiz = null;
+    answer.removeEventListener("input", checkIfAnswerInserted);
+    clearTimeout(timer);
+    resetStopwatch(stopwatch);
+    printTime();
+}
+
+function breakQuiz() {
+    resetQuiz();
+    document.getElementById("BodyQuizPage").style.display = "none";
+    document.getElementById("BodyMainPage").style.display = "grid";
+}
+
+function backToMainPageAfterSavingResults() {
+    resetQuiz();
+    document.getElementById("BodyQuizSummaryPage").style.display = "none";
+    document.getElementById("BodyMainPage").style.display = "grid";
+}
+
+function saveResultWithStatistic() {
+    // TODO
+    backToMainPageAfterSavingResults();
+}
+
+function saveOnlyResult() {
+    // TODO
+    backToMainPageAfterSavingResults();
 }
 
 function nextQuestionFunc() {
     if (currQuestion == 1)
         prevQuestButton.style.display = "initial";
     currQuestion++;
-    printQuestionNumber();
+    //printQuestionNumber();
     if (currQuestion == totalQuestions)
         nextQuestButton.style.display = "none";
     loadQuestion(currQuestion);
-    setCurrQuestionNotAnswered();
-}aaa
-
-
-let stopwatch : Stopwatch = {minutes: 0, seconds: 0};
-timer(stopwatch);
-question.addEventListener("input", checkIfAnswerInserted);
-loadQuiz();
+}
